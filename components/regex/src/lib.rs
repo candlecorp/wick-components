bf48f6723c7ef241d258179fad65e72ba70e0118
Fawad Shaikh <fawad@candle.dev>
2023-04-20 14:40:26 -0400

    Added regex component

---
+++ components/regex/src/lib.rs
@@ -0,0 +1,66 @@
+use std::cmp::min;
+
+use regex::Regex;
+use wasmrs_guest::*;
+mod wick {
+    wick_component::wick_import!();
+}
+use wick::*;
+
+#[async_trait::async_trait(?Send)]
+impl OpRematch for Component {
+    async fn rematch(
+        mut input: WickStream<String>,
+        mut pattern: WickStream<String>,
+        mut outputs: OpRematchOutputs,
+    ) -> wick::Result<()> {
+        while let (Some(Ok(input)), Some(Ok(pattern_string))) =
+            (input.next().await, pattern.next().await)
+        {
+            println!("Pattern: {}", pattern_string);
+            let re = match Regex::new(&pattern_string) {
+                Ok(re) => re,
+                Err(e) => {
+                    return Err(wick_component::anyhow::anyhow!(
+                        "Invalid Regex Pattern: {}",
+                        e
+                    ))
+                }
+            };
+            let mut matches: Vec<Vec<String>> = Vec::new();
+            //use regex to match the pattern and input and return a vec of matches and captures
+            for cap in re.captures_iter(&input) {
+                let mut captures: Vec<String> = Vec::new();
+                for i in 0..min(cap.len(), 10) {
+                    captures.push(cap[i].to_string());
+                }
+                matches.push(captures);
+            }
+            if matches.len() > 0 {
+                outputs.matches.send(&matches);
+                outputs.result.send(&true);
+            } else {
+                outputs.result.send(&false);
+            }
+        }
+        outputs.result.done();
+        outputs.matches.done();
+        Ok(())
+    }
+}
+
+// here is the component definition
+// - name: match
+// inputs:
+//   - name: input
+//     type: string
+//   - name: pattern
+//     type: string
+// outputs:
+//   - name: result
+//     type: bool
+//   - name: output
+//     type: string
+//   - name: captures
+//     type: { list: { type: string } }
+//     optional: true
