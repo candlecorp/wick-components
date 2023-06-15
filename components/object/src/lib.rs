38d56fbb1d2a5d2e808e64a925f8fe2b4eecb5d3
Fawad Shaikh <fawad@candle.dev>
2023-06-15 17:36:30 -0400

    add http client ip and object

--- components/object/src/lib.rs
+++ components/object/src/lib.rs
@@ -1,6 +1,7 @@
 use serde_json::Value;
 use serde_xml_rs::from_str;
 use std::collections::HashMap;
+use std::collections::LinkedList;
 
 mod wick {
     wick_component::wick_import!();
@@ -11,15 +12,18 @@ use wick::*;
 // Implement the "new" operation
 #[async_trait::async_trait(?Send)]
 impl NewOperation for Component {
-type Error=anyhow::Error;
- type Outputs=new::Outputs; type Config=new::Config;
+    type Error = anyhow::Error;
+    type Outputs = new::Outputs;
+    type Config = new::Config;
     async fn new(
-        mut key: WickStream<String>,
         mut value: WickStream<Value>,
         mut outputs: Self::Outputs,
-        _ctx: Context<Self::Config>,
+        ctx: Context<Self::Config>,
     ) -> anyhow::Result<()> {
-        if let (Some(Ok(key)), Some(Ok(value))) = (key.next().await, value.next().await) {
+        let key = &ctx.config.key;
+        while let Some(value) = value.next().await {
+            let value = propagate_if_error!(value, outputs, continue);
+
             let mut new_object = HashMap::new();
             new_object.insert(key, value);
             let new_object_json = serde_json::to_value(new_object)?;
@@ -33,28 +37,26 @@ type Error=anyhow::Error;
 // Implement the "select" operation
 #[async_trait::async_trait(?Send)]
 impl SelectOperation for Component {
-type Error=anyhow::Error;
- type Outputs=select::Outputs; type Config=select::Config;
+    type Error = anyhow::Error;
+    type Outputs = select::Outputs;
+    type Config = select::Config;
     async fn select(
-        mut object: WickStream<Value>,
+        mut input: WickStream<Value>,
         mut path: WickStream<String>,
         mut outputs: Self::Outputs,
         _ctx: Context<Self::Config>,
     ) -> anyhow::Result<()> {
         while let (Some(Ok(object_value)), Some(Ok(path_string))) =
-            (object.next().await, path.next().await)
+            (input.next().await, path.next().await)
         {
-            let selected_values = select(&object_value, &path_string).map_err(|e| {
-                anyhow::anyhow!("Error selecting value by path: {}", e)
-            })?;
+            let selected_values = select(&object_value, &path_string)
+                .map_err(|e| anyhow::anyhow!("Error selecting value by path: {}", e))?;
 
             if let Some(first_selected_value) = selected_values.first() {
                 outputs.output.send(first_selected_value);
             } else {
                 outputs.output.done();
-                return Err(anyhow::anyhow!(
-                    "No value found at the specified path"
-                ));
+                return Err(anyhow::anyhow!("No value found at the specified path"));
             }
         }
 
@@ -66,8 +68,9 @@ type Error=anyhow::Error;
 
 #[async_trait::async_trait(?Send)]
 impl SerializeOperation for Component {
-type Error=anyhow::Error;
- type Outputs=serialize::Outputs; type Config=serialize::Config;
+    type Error = anyhow::Error;
+    type Outputs = serialize::Outputs;
+    type Config = serialize::Config;
     async fn serialize(
         mut content: WickStream<String>,
         mut content_type: WickStream<String>,
@@ -81,17 +84,15 @@ type Error=anyhow::Error;
             let parsed_content: Value = match content_type_string.as_str() {
                 "application/json" => {
                     let content_str = content_string.as_str();
-                    serde_json::from_str(content_str).map_err(|e| {
-                        anyhow::anyhow!("Error parsing JSON content: {}", e)
-                    })?
+                    serde_json::from_str(content_str)
+                        .map_err(|e| anyhow::anyhow!("Error parsing JSON content: {}", e))?
                 }
                 "application/x-www-form-urlencoded" => {
                     let params = url::form_urlencoded::parse(content_string.as_bytes());
                     let parsed_params: HashMap<String, String> = params.into_owned().collect();
                     // Convert the parsed params to a serde_json::Value
-                    serde_json::to_value(parsed_params).map_err(|e| {
-                        anyhow::anyhow!("Error converting params to JSON: {}", e)
-                    })?
+                    serde_json::to_value(parsed_params)
+                        .map_err(|e| anyhow::anyhow!("Error converting params to JSON: {}", e))?
                 }
                 "application/xml" => {
                     let content_str = content_string.as_str();
@@ -99,9 +100,8 @@ type Error=anyhow::Error;
                         from_str::<HashMap<String, Value>>(content_str).or_else(|_| {
                             // Wrap the content with a root element and try parsing again
                             let wrapped_content = format!("<root>{}</root>", content_str);
-                            from_str(&wrapped_content).map_err(|e| {
-                                anyhow::anyhow!("Error parsing XML content: {}", e)
-                            })
+                            from_str(&wrapped_content)
+                                .map_err(|e| anyhow::anyhow!("Error parsing XML content: {}", e))
                         })?;
 
                     // Flatten the nested "$value" fields
@@ -114,15 +114,13 @@ type Error=anyhow::Error;
                         }
                     }
 
-                    serde_json::to_value(flattened_parsed).map_err(|e| {
-                        anyhow::anyhow!("Error converting XML to JSON: {}", e)
-                    })?
+                    serde_json::to_value(flattened_parsed)
+                        .map_err(|e| anyhow::anyhow!("Error converting XML to JSON: {}", e))?
                 }
                 "text/plain" => {
                     //turn content_string into serde_json::Value
-                    serde_json::to_value(content_string).map_err(|e| {
-                        anyhow::anyhow!("Error converting text to JSON: {}", e)
-                    })?
+                    serde_json::to_value(content_string)
+                        .map_err(|e| anyhow::anyhow!("Error converting text to JSON: {}", e))?
                 }
                 _ => {
                     return Err(anyhow::anyhow!(
@@ -137,6 +135,50 @@ type Error=anyhow::Error;
         }
 
         // Signal that the output stream is done
+        outputs.output.done();
+        Ok(())
+    }
+}
+
+fn extend_object_at_path(root: &mut Value, mut path: LinkedList<&str>, new_value: Value) {
+    if let Some(segment) = path.pop_front() {
+        if !root.as_object().unwrap().contains_key(segment) {
+            root.as_object_mut()
+                .unwrap()
+                .insert(segment.into(), Value::Object(serde_json::Map::new()));
+        }
+        let next = root.get_mut(segment).unwrap();
+        extend_object_at_path(next, path, new_value);
+    } else if let Value::Object(map) = root {
+        if let Value::Object(new_map) = new_value {
+            for (k, v) in new_map {
+                map.insert(k, v);
+            }
+        } else {
+            panic!("New value must be an object");
+        }
+    }
+}
+
+// Implement the "push" operation
+#[async_trait::async_trait(?Send)]
+impl PushOperation for Component {
+    type Error = anyhow::Error;
+    type Outputs = push::Outputs;
+    type Config = push::Config;
+    async fn push(
+        mut input: WickStream<Value>,
+        mut value: WickStream<Value>,
+        mut outputs: Self::Outputs,
+        ctx: Context<Self::Config>,
+    ) -> anyhow::Result<()> {
+        let path = &ctx.config.path;
+        if let (Some(Ok(mut input)), Some(Ok(value))) = (input.next().await, value.next().await) {
+            let path = path.replace("$.", "");
+            let path: LinkedList<&str> = path.trim_start_matches('.').split('.').collect();
+            extend_object_at_path(&mut input, path, value);
+            outputs.output.send(&input);
+        }
         outputs.output.done();
         Ok(())
     }
---
+++ http-client-ip/src/lib.rs
@@ -0,0 +1,37 @@
+mod wick {
+    wick_component::wick_import!();
+}
+use wick::{types::http::HttpRequest, *};
+
+#[async_trait::async_trait(?Send)]
+impl GetIpOperation for Component {
+    type Error = anyhow::Error;
+    type Outputs = get_ip::Outputs;
+    type Config = get_ip::Config;
+
+    async fn get_ip(
+        mut request: WickStream<HttpRequest>,
+        mut outputs: Self::Outputs,
+        _ctx: Context<Self::Config>,
+    ) -> Result<(), Self::Error> {
+        while let Some(request) = request.next().await {
+            let request = propagate_if_error!(request, outputs, continue);
+            let xff = request
+                .headers
+                .get("x-forwarded-for")
+                .and_then(|x| Some(x[0].as_str()))
+                .unwrap_or("");
+            if xff.is_empty() {
+                outputs.ip.send(&request.remote_addr);
+                continue;
+            } else {
+                let ips = xff.split(',').collect::<Vec<_>>();
+                let ip = ips[0].trim();
+                outputs.ip.send(&ip.to_string());
+                continue;
+            }
+        }
+        outputs.ip.done();
+        Ok(())
+    }
+}
