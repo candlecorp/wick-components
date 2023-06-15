7e2044ae5d349a7ba327e40ae5959760a52762c8
Jarrod Overson <jsoverson@gmail.com>
2023-06-15 17:38:44 -0400

    Added hello-world component, added publish task, updated components (#11)
    * feat: added hello-world component
    
    * added hello world, updated tasks, added publish task
    
    * feat: add capture operation
    
    * test: added additional test for url
    
    ---------
    
    Co-authored-by: fawadasaurus <99134191+fawadasaurus@users.noreply.github.com>
    Co-authored-by: Fawad Shaikh <fawad@candle.dev>

--- components/rand/src/lib.rs
+++ components/rand/src/lib.rs
@@ -1,29 +1,24 @@
-
 mod wick {
     wick_component::wick_import!();
 }
-use rand::prelude::*;
-use rand::rngs::SmallRng;
-use rand::SeedableRng;
 
 use wick::*;
 
 #[async_trait::async_trait(?Send)]
 impl IntOperation for Component {
-type Error=anyhow::Error;
- type Outputs=int::Outputs; type Config=int::Config;
+    type Error = anyhow::Error;
+    type Outputs = int::Outputs;
+    type Config = int::Config;
     async fn int(
         mut min: WickStream<u32>,
         mut max: WickStream<u32>,
         mut outputs: Self::Outputs,
         ctx: Context<Self::Config>,
     ) -> anyhow::Result<()> {
-        let seed = ctx.inherent.as_ref().expect("no seed available").seed;
         while let (Some(Ok(min)), Some(Ok(max))) = (min.next().await, max.next().await) {
             println!("Received min: {}, max: {}", min, max);
-            let mut rng = SmallRng::seed_from_u64(seed);
-            let rando = rng.gen_range(min..max);
-            outputs.output.send(&(rando));
+            let num: u32 = ctx.inherent.rng.gen();
+            outputs.output.send(&num);
         }
         outputs.output.done();
         Ok(())
