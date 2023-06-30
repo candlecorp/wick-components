f31e79833a269b40c4a01e414f2b0dfa08b61bc5
Jarrod Overson <jsoverson@gmail.com>
2023-06-30 17:54:30 -0400

    feat: improved substream handling of unzip

--- components/unzip/src/lib.rs
+++ components/unzip/src/lib.rs
@@ -8,6 +8,7 @@ mod wick {
 }
 
 use wick::*;
+use wick_component::packet::Packet;
 
 #[cfg_attr(target_family = "wasm",async_trait::async_trait(?Send))]
 #[cfg_attr(not(target_family = "wasm"), async_trait::async_trait)]
@@ -17,29 +18,67 @@ impl UnzipOperation for Component {
     type Config = unzip::Config;
 
     async fn unzip(
-        mut input: WickStream<Bytes>,
-        mut outputs: Self::Outputs,
+        input: WickStream<Packet>,
+        outputs: Self::Outputs,
         _ctx: Context<Self::Config>,
     ) -> Result<(), Self::Error> {
-        let mut zip = stream_unzip::ZipReader::new();
-        while let Some(input) = input.next().await {
-            let input = propagate_if_error!(input, outputs, continue);
-            println!("got {} bytes", input.len());
+        let (_, mut outputs) = handle_new_stream(input, outputs, &_ctx, true).await;
+        outputs.filename.done();
+        outputs.contents.done();
+
+        Ok(())
+    }
+}
+
+#[cfg_attr(not(target_family = "wasm"), async_recursion::async_recursion)]
+#[cfg_attr(target_family = "wasm", async_recursion::async_recursion(?Send))]
+async fn handle_new_stream(
+    mut input_stream: WickStream<Packet>,
+    mut outputs: unzip::Outputs,
+    _ctx: &Context<unzip::Config>,
+    start: bool,
+) -> (WickStream<Packet>, unzip::Outputs) {
+    let mut zip = stream_unzip::ZipReader::new();
+    if start {
+        outputs.broadcast_open();
+    }
+
+    while let Some(input) = input_stream.next().await {
+        let input = propagate_if_error!(input, outputs, continue);
+        if input.is_open_bracket() {
+            if !start {
+                outputs.broadcast_open();
+            }
+            (input_stream, outputs) = handle_new_stream(input_stream, outputs, _ctx, false).await;
+            if !start {
+                outputs.broadcast_close();
+            }
+        } else if input.is_close_bracket() || input.is_done() {
+            break;
+        } else {
+            println!("acting on packet: {:?}", input);
+            if !input.has_data() {
+                continue;
+            }
+            let input: Bytes = propagate_if_error!(input.decode(), outputs, continue);
+
             zip.update(input.into());
             let entries = zip.drain_entries();
             for entry in entries {
                 let expanded = entry.inflate().unwrap();
 
                 let filename = expanded.name().to_owned();
+
                 let (_, data) = expanded.into_parts();
-                println!("got entry {}", filename);
                 outputs.filename.send(&filename);
                 outputs.contents.send(&Bytes::new(data));
             }
         }
-
-        outputs.filename.done();
-        outputs.contents.done();
-        Ok(())
     }
+
+    if start {
+        outputs.broadcast_close();
+    }
+
+    (input_stream, outputs)
 }
