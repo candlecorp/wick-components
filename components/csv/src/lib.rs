31487047d5a18190bf17862e6b3b0062dbde4f25
Jarrod Overson <jsoverson@gmail.com>
2023-07-01 06:34:34 -0400

    feat: improved csv substream handling

--- components/csv/src/lib.rs
+++ components/csv/src/lib.rs
@@ -6,9 +6,9 @@ mod wick {
     )]
     wick_component::wick_import!();
 }
-use std::ops::Deref;
 
 use wick::*;
+use wick_component::{packet::Packet, Bytes};
 
 #[cfg_attr(target_family = "wasm",async_trait::async_trait(?Send))]
 #[cfg_attr(not(target_family = "wasm"), async_trait::async_trait)]
@@ -18,27 +18,74 @@ impl ParseBytesOperation for Component {
     type Config = parse_bytes::Config;
 
     async fn parse_bytes(
-        mut input: WickStream<Bytes>,
-        mut outputs: Self::Outputs,
+        input: WickStream<Packet>,
+        outputs: Self::Outputs,
         _ctx: Context<Self::Config>,
     ) -> Result<(), Self::Error> {
-        while let Some(input) = input.next().await {
-            let input = propagate_if_error!(input, outputs, continue);
-            let input: &[u8] = input.deref();
-            let mut rdr = csv::ReaderBuilder::new()
-                .has_headers(false)
-                .from_reader(input);
-
-            outputs.output.open_bracket();
-            for result in rdr.records() {
-                let record = result?;
-
-                let fields: Vec<String> = record.deserialize(None)?;
-                outputs.output.send(&fields);
-            }
-            outputs.output.close_bracket();
-        }
+        let (_, mut outputs) = handle_new_stream(input, outputs, &_ctx, true).await;
         outputs.output.done();
+
         Ok(())
     }
+}
+
+#[cfg_attr(not(target_family = "wasm"), async_recursion::async_recursion)]
+#[cfg_attr(target_family = "wasm", async_recursion::async_recursion(?Send))]
+async fn handle_new_stream(
+    mut input_stream: WickStream<Packet>,
+    mut outputs: parse_bytes::Outputs,
+    _ctx: &Context<parse_bytes::Config>,
+    start: bool,
+) -> (WickStream<Packet>, parse_bytes::Outputs) {
+    if start {
+        outputs.broadcast_open();
+    }
+
+    while let Some(input) = input_stream.next().await {
+        let input = propagate_if_error!(input, outputs, continue);
+        if input.is_open_bracket() {
+            if !start {
+                outputs.broadcast_open();
+            }
+            (input_stream, outputs) = handle_new_stream(input_stream, outputs, _ctx, false).await;
+            if !start {
+                outputs.broadcast_close();
+            }
+        } else if input.is_close_bracket() || input.is_done() {
+            break;
+        } else {
+            println!("acting on packet: {:?}", input);
+            if !input.has_data() {
+                continue;
+            }
+            let input: Bytes = propagate_if_error!(input.decode(), outputs, continue);
+            if let Err(e) = handle_packet(&input, &mut outputs).await {
+                outputs.output.error(e.to_string());
+            }
+        }
+    }
+
+    if start {
+        outputs.broadcast_close();
+    }
+
+    (input_stream, outputs)
+}
+
+async fn handle_packet(
+    input: &Bytes,
+    outputs: &mut parse_bytes::Outputs,
+) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let mut rdr = csv::ReaderBuilder::new()
+        .has_headers(false)
+        .from_reader(input.as_ref());
+
+    for result in rdr.records() {
+        let record = result.unwrap();
+
+        let fields: Vec<String> = record.deserialize(None).unwrap();
+        outputs.output.send(&fields);
+    }
+
+    Ok(())
 }
