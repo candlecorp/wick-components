5c8ab8985a3aeafc41bc2e1b32a097de1f830b5a
Jarrod Overson <jsoverson@gmail.com>
2023-08-08 15:20:29 -0400

    feat: updated liquid to support passing in simple values as the context

--- components/liquid/src/lib.rs
+++ components/liquid/src/lib.rs
@@ -6,48 +6,25 @@ mod wick {
 }
 use wick::*;
 
-#[cfg_attr(target_family = "wasm",async_trait::async_trait(?Send))]
-#[cfg_attr(not(target_family = "wasm"), async_trait::async_trait)]
-impl render::Operation for Component {
-    type Error = anyhow::Error;
-    type Outputs = render::Outputs;
-    type Config = render::Config;
-    async fn render(
-        mut input: WickStream<Value>,
-        mut outputs: Self::Outputs,
-        ctx: Context<Self::Config>,
-    ) -> anyhow::Result<()> {
-        let template_string = ctx.config.template.clone();
-        println!("Template: {:?}", template_string);
+#[wick_component::operation(unary_simple)]
+fn render(input: Value, ctx: Context<render::Config>) -> Result<String, anyhow::Error> {
+    // Create a Liquid parser
+    let parser = ParserBuilder::with_stdlib().build().unwrap();
 
-        // Create a Liquid parser
-        let parser = ParserBuilder::with_stdlib().build().unwrap();
+    // Parse the template string
+    let liquid_template = match parser.parse(&ctx.config.template) {
+        Ok(t) => t,
+        Err(e) => return Err(anyhow::anyhow!("Invalid template string: {}", e)),
+    };
 
-        while let Some(Ok(input)) = input.next().await {
-            // Parse the template string
-            let liquid_template = match parser.parse(&template_string) {
-                Ok(t) => t,
-                Err(e) => return Err(anyhow::anyhow!("Invalid template string: {}", e)),
-            };
+    let context = liquid::object!({"input": input});
 
-            println!("params: {:?}", input);
+    // Render the template with the provided parameters
+    let rendered = match liquid_template.render(&context) {
+        Ok(r) => r,
+        Err(e) => return Err(anyhow::anyhow!("Error rendering template: {}", e)),
+    };
 
-            let globals = liquid::object!(&input);
-
-            println!("globals: {:?}", globals);
-
-            // Render the template with the provided parameters
-            let rendered = match liquid_template.render(&globals) {
-                Ok(r) => r,
-                Err(e) => return Err(anyhow::anyhow!("Error rendering template: {}", e)),
-            };
-
-            // Send the rendered string to the output
-            outputs.output.send(&rendered);
-        }
-
-        // Signal that the output stream is done
-        outputs.output.done();
-        Ok(())
-    }
+    // Send the rendered string to the output
+    Ok(rendered)
 }
