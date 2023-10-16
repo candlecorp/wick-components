c20a93d57994a8b49c445188bfd5190e9594a75b
Jarrod Overson <jsoverson@gmail.com>
2023-10-16 18:00:23 -0400

    feat: made yolo a better component citizen, added yolofs composite

--- components/yolo/src/lib.rs
+++ components/yolo/src/lib.rs
@@ -9,25 +9,16 @@ use wick::*;
 
 use model::Model;
 
-#[wick_component::operation(generic_raw)]
-async fn detect(
-    mut inputs: detect::Inputs,
-    mut outputs: detect::Outputs,
-    ctx: Context<detect::Config>,
-) -> Result<()> {
+#[wick_component::operation(unary_simple)]
+async fn detect(image: Bytes, ctx: Context<detect::Config>) -> Result<Value> {
     let bytes = std::fs::read(&ctx.root_config().model)?;
     let model = Model::new(bytes, "n")?;
-    while let Some(image) = inputs.image_data.next().await {
-        let image = propagate_if_error!(image.decode(), outputs, continue);
 
-        let results = model.run(
-            image.into(),
-            ctx.config.confidence.unwrap_or(0.5),
-            ctx.config.iou.unwrap_or(0.5),
-        )?;
+    let results = model.run(
+        image.into(),
+        ctx.config.confidence.unwrap_or(0.5),
+        ctx.config.iou.unwrap_or(0.5),
+    )?;
 
-        outputs.output.send(&serde_json::to_value(&results)?);
-    }
-
-    Ok(())
+    Ok(serde_json::to_value(&results)?)
 }
